8c97820b545df501bb292edf7d73234af89514cf
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2024-07-16 14:43:01 +0530

    error fixed

--- product/components/ui/canvas-reveal-effect.tsx
+++ product/components/ui/canvas-reveal-effect.tsx
@@ -2,8 +2,8 @@
 import { cn } from "@/lib/utils";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
-// import * as THREE from "three";
-import * as THREE from "three"
+import * as THREE from "three";
+
 
 export const CanvasRevealEffect = ({
   animationSpeed = 0.4,
