7f7aca29d212d8293f9435a14026d253cbe390d8
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2024-07-18 13:14:40 +0530

    ch

--- product/components/Hero.tsx
+++ product/components/Hero.tsx
@@ -21,7 +21,7 @@ const wordss = [
   ];
 export default function BackgroundBeamsDemo() {
   return (
-    <div className="h-[50rem] w-full bg-gray-950 relative flex items-center justify-center  antialiased p-10 ml-20">
+    <div className="h-[50rem] w-full bg-gray-950 relative flex items-center justify-center  antialiased p-10 ml-10">
       <div className="max-w-6xl mx-auto p-4 flex flex-col lg:flex-row items-center lg:items-start justify-between ">
         <div className="lg:w-1/2">
           <h1 className="text-white text-2xl font-['Bricolage_Grotesque']">Hey, I'm</h1>
