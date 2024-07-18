3735c43f36dfef5e89cb4d3244f9ecb1e27bb24c
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2024-07-18 14:01:11 +0530

    error fixed

--- product/next.config.mjs
+++ product/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    distDir: 'dist',
     images: {
         domains: ['img.icons8.com', 'gramentheme.com', 'img.freepik.com', 'images.unsplash.com'],
       },
